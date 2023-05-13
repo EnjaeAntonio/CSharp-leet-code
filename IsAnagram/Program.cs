93291ab9f8f5b8943256e90e3bb91c216732b345
Enjae Antonio <enjaeantonio@outlook.com>
2023-05-13 16:17:57 -0500

    Fixed IsAnagram solution

--- IsAnagram/Program.cs
+++ IsAnagram/Program.cs
@@ -3,26 +3,24 @@
     public Solution() { }
     public bool IsAnagram(string s, string t)
     {
-        HashSet<int> countS = new HashSet<int>();
-        HashSet<int> countT = new HashSet<int>();
-        if (s.Length != t.Length)
+        if (s.Length != t.Length) return false;
+        if (s == t) return true;
+
+        Dictionary<char, int> sCounts = new Dictionary<char, int>();
+        Dictionary<char, int> tCounts = new Dictionary<char, int>();
+
+        for(int i  = 0; i < s.Length; i++)
         {
-            return false;
+            sCounts[s[i]] = 1 + (sCounts.ContainsKey(s[i]) ? sCounts[s[i]] : 0);
+            tCounts[t[i]] = 1 + (tCounts.ContainsKey(t[i]) ? tCounts[t[i]] : 0);
         }
-        else
+
+        foreach (char c in sCounts.Keys)
         {
-            for (int i = 0; i < s.Length; i++)
-            {
-                countS.Add(s[i]);
-                countT.Add(t[i]);
-                bool haveSameValues = countS.SetEquals(countT);
-                if(haveSameValues)
-                {
-                    return true;
-                }
-            }
+            int tCount = tCounts.ContainsKey(c) ? tCounts[c] : 0;
+            if (sCounts[c] != tCount) return false;
         }
-        return false;
+        return true;
     }
 }
 
